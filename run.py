1ef106543d169e021a099d46c7985314ee6be367
钟智强 <johnmelodymel@qq.com>
2025-02-06 02:49:13 +0800

    `Removed src/__init__.py and added comments to run.py for PyInstaller build`

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
 import uvicorn
 
+# To build executable
+# pyinstaller --onefile --windowed --name YYAI {SCRIPT}t.py
+
 if __name__ == "__main__":
     uvicorn.run("src.app:app", host="0.0.0.0", port=8000, reload=True)
